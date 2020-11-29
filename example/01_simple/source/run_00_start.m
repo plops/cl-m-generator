731a7c1ae37f7832e8e187456e9670b26d05b256
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-11-29 21:53:02 +0100

    first example

---
+++ example/01_simple/source/run_00_start.m
@@ -0,0 +1,5 @@
+_code_git_version="82418eba15c2fa64539b2da8ce3e78a72f5c2304";
+_code_repository="https://github.com/plops/cl-m-generator/tree/master/example/01_simple/source/run_00_start.m";
+_code_generation_time="21:52:34 of Sunday, 2020-11-29 (GMT+1)";
+
+
